46f941d90d88d68ac28ddcac3a4f3200f923debb
m-vp <bl.en.u4cse22236@bl.students.amrita.edu>
2026-05-07 11:28:09 +0530

    path change for dns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,31 +9,31 @@
 
 
 
-// const nextConfig = {
-//   output: "standalone",
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/backend/:path*",
-//         destination: "http://backend.internal:5000/:path*",
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-
-
 const nextConfig = {
   output: "standalone",
   async rewrites() {
     return [
       {
         source: "/backend/:path*",
-        destination: "http://localhost:5000/:path*",
+        destination: "http://backend.internal:5000/:path*",
       },
     ];
   },
 };
 
 export default nextConfig;
+
+
+// const nextConfig = {
+//   output: "standalone",
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/backend/:path*",
+//         destination: "http://localhost:5001/:path*",
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
